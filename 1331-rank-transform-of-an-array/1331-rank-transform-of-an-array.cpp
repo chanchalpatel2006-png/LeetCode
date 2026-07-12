0e85f7f38613289542feb5f565826b8aed027341
Chanchal <chanchalpatel2006@gmail.com>
2026-07-13 01:02:23 +0530

    Time: 36 ms (72.49%), Space: 44 MB (55.31%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        vector<int> copy=arr;
-        sort(copy.begin(),copy.end());
-        int n=arr.size(),rank=1;
-        if(n==0) return arr;
-        unordered_map<int,vector<int>> hash;
-        for(int i=0;i<n;i++){
-            hash[arr[i]].push_back(i);
+        vector<int> copy = arr;
+        sort(copy.begin(), copy.end());
+
+        unordered_map<int, int> rank;
+        int r = 1;
+
+        // Assign ranks to unique values
+        for (int x : copy) {
+            if (!rank.count(x))
+                rank[x] = r++;
         }
-        for(int i=1;i<n;i++){
-            if(copy[i]!=copy[i-1]){
-                rank++;
-            }
-            for(int j:hash[copy[i]])
-                arr[j]=rank;
-        }
-        for(int j:hash[copy[0]])
-                arr[j]=1;
+
+        // Replace each element with its rank
+        for (int &x : arr)
+            x = rank[x];
 
         return arr;
     }
