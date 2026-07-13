1e547ada6d3a3ff1c27a3b9758525cb402f113a2
Chanchal <chanchalpatel2006@gmail.com>
2026-07-14 01:31:53 +0530

    Time: 0 ms (100%), Space: 8.3 MB (76.62%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,44 +1,20 @@
 class Solution {
 public:
-    vector<int> ans;
-    void generateNumber(int first_digit,int len,int high){
-        if(first_digit+len-1>9){
-            first_digit=1;
-            len++;
-        }
-        long long num=first_digit;
-        int next_digit=first_digit+1;
-        for(int i=0;i<len-1;i++){
-            num=num*10+next_digit;
-            next_digit++;
-        }
-        if(num<=high){
-            ans.push_back(num);
-        }
-        else{
-            return;
-        }
-        first_digit++;
-        if(num%10==9){
-            len++;
-            first_digit=1;
-        }
-        
-        generateNumber(first_digit,len,high);
-
-    }
     vector<int> sequentialDigits(int low, int high) {
-        int len=0,l=low,first_digit=0;
-        while(l>0){
-            first_digit=l%10;
-            l=l/10;
-            len++;
+        vector<int> ans;
+
+        for (int len = 2; len <= 9; len++) {
+            for (int start = 1; start + len - 1 <= 9; start++) {
+                int num = 0;
+
+                for (int d = start; d < start + len; d++)
+                    num = num * 10 + d;
+
+                if (num >= low && num <= high)
+                    ans.push_back(num);
+            }
         }
-        generateNumber(first_digit,len,high);
-        if(!ans.empty() && ans[0]<low){
-            ans.erase(ans.begin());
-        }
-        return ans;    
+
+        return ans;
     }
-    
 };
