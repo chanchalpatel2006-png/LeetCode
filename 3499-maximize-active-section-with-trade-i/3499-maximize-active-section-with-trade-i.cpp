0e4a51e8bc0bc46569142f4538229577b2cf3c2d
Chanchal <chanchalpatel2006@gmail.com>
2026-07-21 21:16:40 +0530

    Time: 75 ms (48.65%), Space: 54.9 MB (76.13%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
-        int n=s.size(),active=0,zero=0;
-        vector<int> Z;
-        for(int i=0;i<n;i++){
-            if(s[i]=='1'){
+        int n = s.size(), active = 0, zero = 0;
+        int mx = 0;
+        int prev = 0, curr = 0;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '1') {
                 active++;
             }
         }
-        for(int i=0;i<n;i++){
-            if(s[i]=='0'){
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '0') {
                 zero++;
-            }
-            else if(s[i]=='1' && zero>0){
-                Z.push_back(zero);
-                zero=0;
+            } else if (s[i] == '1' && zero > 0 ) {
+                curr = zero;
+                if(prev>0) mx = max(mx, prev + curr);
+                prev = curr;
+                zero = 0;
             }
         }
-        if(zero>0)  Z.push_back(zero);
-        int mx=0;
-        if(Z.size()<2) return active;
-        for(int i=0;i<Z.size()-1;i++){
-            mx=max(mx,Z[i]+Z[i+1]);
+        if (zero > 0) {
+            curr = zero;
+            if(prev>0) mx = max(mx, prev + curr);
+            prev = curr;
+            zero = 0;
         }
-        return active+mx;
-
-        
+        return active + mx;
     }
 };
