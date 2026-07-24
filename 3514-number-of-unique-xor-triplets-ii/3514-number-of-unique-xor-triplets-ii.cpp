7656a2c30c0a9c7c993522bc028544636402f00d
Chanchal <chanchalpatel2006@gmail.com>
2026-07-25 00:33:11 +0530

    Time: 639 ms (64.61%), Space: 35.6 MB (77.53%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
-        int n=nums.size();
-        int base=1;
-        int mx=*max_element(nums.begin(),nums.end());
-        while(base<=mx){
-            base=base<<1;
-        }
-        vector<bool> xor2(base,false);
-        vector<bool> xor3(base,false);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                xor2[nums[i]^nums[j]]=true;
+        const int B = 2048;
+
+        vector<bool> one(B, false);
+        for (int x : nums)
+            one[x] = true;
+
+        vector<bool> two(B, false);
+
+        for (int a = 0; a < B; a++) {
+            if (!one[a]) continue;
+
+            for (int b = 0; b < B; b++) {
+                if (one[b])
+                    two[a ^ b] = true;
             }
         }
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<base;j++){
-                if(xor2[j]){
-                    xor3[nums[i]^j]=true;
-                }
+        vector<bool> three(B, false);
+
+        for (int x = 0; x < B; x++) {
+            if (!one[x]) continue;
+
+            for (int y = 0; y < B; y++) {
+                if (two[y])
+                    three[x ^ y] = true;
             }
         }
 
-
-        return count(xor3.begin(),xor3.end(),true);
-        
+        return count(three.begin(), three.end(), true);
     }
 };
