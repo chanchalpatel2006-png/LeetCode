0473c5ce6c4d09e37e632236ba9696cd8e131b0f
Chanchal <chanchalpatel2006@gmail.com>
2026-08-06 00:12:49 +0530

    Time: 643 ms (8.97%), Space: 301.5 MB (78.62%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        vector<vector<int>> adj(n);
+        vector<int> inDeg(n,0);
+        vector<bool> sus(n,false);
+        for(auto &edge:invocations){
+            adj[edge[0]].push_back(edge[1]);
+            inDeg[edge[1]]++;
+        }
+        //bfs
+        queue<int> q;
+        q.push(k);
+        sus[k]=true;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            for(auto it:adj[node]){
+                inDeg[it]--;
+                if(!sus[it]){
+                    sus[it]=true;
+                    q.push(it);
+                }
+            }
+        }
+        vector<int> result;
+        bool cannotRemove=false;
+        for(int i=0;i<n;i++){
+            if(sus[i] && inDeg[i]>0){
+                cannotRemove=true;
+                break;
+            }
+            else if(!sus[i]){
+                result.push_back(i);
+            }
+        }
+        if(cannotRemove){
+            vector<int> vec(n);
+            for(int j=0;j<n;j++){
+                vec[j]=j;
+            }
+            return vec;
+        }
+        return result;
+
+
+
+        
+    }
+};
