89ef7171e1db575afd0b2010e7bb9b183a498ca5
Chanchal <chanchalpatel2006@gmail.com>
2026-09-08 18:17:23 +0530

    Time: 3 ms (25.64%), Space: 17.1 MB (38.97%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    int bfs(int row, int col, vector<vector<int>>& vis,
+            vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size(), minute = 0, min = 0;
+        queue<pair<int, int>> q;
+        q.push({row, col});
+        vis[row][col] = 1;
+        vector<pair<int, int>> neighbours = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+        while (!q.empty()) {
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+            min = 0;
+            for (auto [dRow, dCol] : neighbours) {
+                int nrow = dRow + r;
+                int ncol = dCol + c;
+                if (nrow >= 0 && ncol >= 0 && nrow < m && ncol < n &&
+                    grid[nrow][ncol] == 1 && !vis[nrow][ncol]) {
+                    vis[nrow][ncol] = 1;
+                    grid[nrow][ncol] = 2;
+                    q.push({nrow, ncol});
+                    min = 1;
+                }
+            }
+            minute += min;
+        }
+        return minute;
+    }
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size(), minute = 0;
+        queue<pair<pair<int, int>,int>> q;
+        int mx=0;
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (!vis[row][col] && grid[row][col] == 2) {
+                    q.push({{row, col}, 0});
+                }
+            }
+        }
+
+        vector<pair<int, int>> neighbours = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+        while (!q.empty()) {
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+            int t= q.front().second;
+            q.pop();
+            mx=max(mx,t);
+            for (auto [dRow, dCol] : neighbours) {
+                int nrow = dRow + r;
+                int ncol = dCol + c;
+                if (nrow >= 0 && ncol >= 0 && nrow < m && ncol < n &&
+                    grid[nrow][ncol] == 1 && !vis[nrow][ncol]) {
+                    vis[nrow][ncol] = 1;
+                    grid[nrow][ncol] = 2;
+                    q.push({{nrow, ncol},t+1});
+                }
+            }
+        }
+
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (grid[row][col] == 1) {
+                    return -1;
+                }
+            }
+        }
+
+        return mx;
+    }
+};
