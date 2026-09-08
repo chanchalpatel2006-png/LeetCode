a80e29f5e192a4bb4ff7b802426495074a4bd87d
Chanchal <chanchalpatel2006@gmail.com>
2026-09-08 16:35:31 +0530

    Time: 89 ms (5.11%), Space: 41.9 MB (5.02%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,47 +1,41 @@
 class Solution {
 public:
+    void bfs(int row,int col,vector<vector<int>>&vis,vector<vector<char>>&grid){
+        int n=grid.size(),m=grid[0].size();
+        vis[row][col]=1;
+        queue<pair<int,int>> q;
+        q.push({row,col});
+        vector<vector<int>>neighbours={{0,1},{1,0},{0,-1},{-1,0}};
+        while(!q.empty()){
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            for(auto neighbour:neighbours){
+                int nrow=neighbour[0]+row;
+                int ncol=neighbour[1]+col;
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1' && !vis[nrow][ncol]){
+                    vis[nrow][ncol]=1;
+                    q.push({nrow,ncol});
+
+                }
+            
+            }
+        }
+
+    }
     int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> adj(m * n);
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int node = (i * n) + (j);
-                if (grid[i][j] == '1') {
-                    if (i - 1 >= 0 && grid[i - 1][j] == '1') {
-                        adj[node].push_back(node - n);
-                        adj[node - n].push_back(node);
-                    }
-                    if (j - 1 >= 0 && grid[i][j - 1] == '1') {
-                        adj[node].push_back(node - 1);
-                        adj[node - 1].push_back(node);
-                    }
+        int n=grid.size(),m=grid[0].size();
+        int cnt=0;
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        for(int row=0;row<n;row++){
+            for(int col=0;col<m;col++){
+                if(!vis[row][col] && grid[row][col]=='1'){
+                    cnt++;
+                    bfs(row,col,vis,grid);
                 }
             }
         }
-        vector<int> vis(m * n, 0);
-        int islands = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if(grid[i][j]=='0') continue;
-                if (vis[i*n+j])
-                    continue;
-                islands++;
-                
-                queue<int> q;
-                q.push(i*n+j);
-                vis[i*n+j] = 1;
-                while (!q.empty()) {
-                    int node = q.front();
-                    q.pop();
-                    for (auto it : adj[node]) {
-                        if (vis[it])
-                            continue;
-                        vis[it] = 1;
-                        q.push(it);
-                    }
-                }
-            }
-        }
-        return islands;
+        return cnt;
+        
     }
 };
